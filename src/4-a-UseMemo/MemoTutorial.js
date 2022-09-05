846515d93e1eaf3035e19cf060ecf2301a3ce81a
Russell Padgett <russpadgett@gmail.com>
2022-09-04 23:13:10 -0500

    memo update

--- src/4-a-UseMemo/MemoTutorial.js
+++ src/4-a-UseMemo/MemoTutorial.js
@@ -4,10 +4,9 @@ import { useEffect, useState, useMemo } from "react";
 export default function MemoTutorial() {
   const [data, setData] = useState(null);
   const [toggle, setToggle] = useState(false);
-  const [toggle2, setToggle2] = useState(false);
-
+  
   useEffect(() => {
-    console.log('comments api was called');
+    console.log("comments api was called");
     axios
       .get("https://jsonplaceholder.typicode.com/comments")
       .then((response) => {
@@ -15,48 +14,34 @@ export default function MemoTutorial() {
       });
   }, []);
 
-  const findLongestName = (comments) => {
-    if(!comments)
-      return '';
-    else{
-      console.log(comments[0])
-      const nameObj = comments.reduce((o={},v,i) => {
-        if(v?.name?.length > o?.name?.length)
-          o.name = v.name;  
-        return o;
-      },{})
-      console.log(nameObj)
-      return nameObj.name
-    }
+  const findLongestName1 = (comments) => {
+    console.log('findLongestName1 calculated')
+    return comments?.reduce((o, v, i) => {
+      if (o.name.length < v.name.length) o.name = v.name;
+      return o;
+    }).name;
   };
 
-  // const findLongestName = (comments) => {
-  //   if (!comments) return null;
+  const findLongestName2 = (comments) => {
+    console.log('findLongestName2 calculated')
+    return comments?.reduce((o, v, i) => {
+      if (o.name === undefined) o.name = "";
+      if (o.name.length < v.name.length) o.name = v.name;
+      return o;
+    }, {}).name;
+  };
 
-  //   let longestName = "";
-  //   for (let i = 0; i < comments.length; i++) {
-  //     let currentName = comments[i].name;
-  //     if (currentName.length > longestName.length) {
-  //       longestName = currentName;
-  //     }
-  //   }
-
-  //   console.log("THIS WAS COMPUTED");
-
-  //   return longestName;
-  // };
-
-  const getLongestName = useMemo(() => findLongestName(data), [data]);
+  const getLongestName1 = useMemo(() => findLongestName1(data), [data]);
+  const getLongestName2 = findLongestName2(data);
 
   return (
     <div className="App">
       <h1>axios, useEffect, useState, useMemo</h1>
       <h2>Cache result</h2>
-      <div> {getLongestName} </div>
+      <div> {getLongestName1} </div>
+      <div> {getLongestName2} </div>
       <button onClick={() => { setToggle(!toggle); }}> Toggle </button>
       {toggle && <h1> toggle </h1>}
-      <button onClick={() => { setToggle2(!toggle2); }}> Toggle 2 </button>
-      {toggle2 && <h1> toggle 2 </h1>}
     </div>
   );
 }
